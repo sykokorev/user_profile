54220edca4da9dffd081bc02c9eea4c8b73805d3
kokorev.s <skokorev1981@gmail.com>
2022-05-17 14:20:30 +0300

    Initiate trainig app

---
+++ training/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from models import Exercise, Equipment, Level, ExerciseType
+
+
+admin.site.register(Exercise)
+admin.site.register(Equipment)
+admin.site.register(Level)
+admin.site.register(ExerciseType)
---
+++ training/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class ExerciseType(models.Model):
+    pass
+
+
+class Level(models.Model):
+    pass
+
+
+class Equipment(models.Model):
+
+    pass
+
+
+class Exercise(models.Model):
+
+    pass
